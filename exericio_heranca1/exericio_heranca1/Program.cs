0e5cd088d530d7f7aefa36cc8e093721107749f6
tmendes <th.mendes1997@gmail.com>
2019-05-01 20:18:06 -0300

    Inicio

---
+++ exericio_heranca1/exericio_heranca1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exericio_heranca1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
