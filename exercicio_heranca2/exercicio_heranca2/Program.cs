86437bf71122052248dfc07af0442456b4407730
tmendes <th.mendes1997@gmail.com>
2019-05-05 21:14:03 -0300

    config

---
+++ exercicio_heranca2/exercicio_heranca2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exercicio_heranca2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
