ef9ce92518d50682b040b40097b3c36e65b94fb4
tmendes <th.mendes1997@gmail.com>
2019-05-07 18:32:58 -0300

    Inicio

---
+++ exercicio_polimorfismo/exercicio_polimorfismo/Entities/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace exercicio_polimorfismo.Entities
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public int Hours { get; set; }
+        public double valuePerHour { get; set; }
+
+        public double Payment()
+        {
+            return Hours * valuePerHour;
+        }
+    }
+}
---
+++ exercicio_polimorfismo/exercicio_polimorfismo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exercicio_polimorfismo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
