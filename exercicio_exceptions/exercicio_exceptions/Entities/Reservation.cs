a1669250c21d764e533da9274c20f37271eefdfb
tmendes <th.mendes1997@gmail.com>
2019-05-20 17:57:22 -0300

    Exceptions

---
+++ exercicio_exceptions/exercicio_exceptions/Entities/Reservation.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using exercicio_exceptions.Entities.Exceptions;
+
+namespace exercicio_exceptions.Entities
+{
+    class Reservation
+    {
+        public int RoomNumber { get; set; }
+
+        public DateTime CheckIn { get; set; }
+
+        public DateTime CheckOut { get; set; }
+
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            if (checkOut <= checkIn)
+            {
+                throw new DomainException("O check-out não pode ser em uma data anterior que o check-in");
+            }
+
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+
+            return (int)duration.TotalDays;
+        }
+
+        public void UpdateDate(DateTime checkIn, DateTime checkOut)
+        {
+            DateTime now = DateTime.Now;
+            if (checkIn < now || checkOut < now)
+            {
+                throw new DomainException("Para atualização de data as mesmas devem ser futuras.");
+            }
+
+            if (checkOut <= checkIn)
+            {
+                throw new DomainException("O check-out não pode ser em uma data anterior que o check-in");
+            }
+
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+
+        }
+
+        public override string ToString()
+        {
+            string reservation = String.Format("Reserva: Quarto {0}, check-in: {1}, check-out: {2}, {3} noites."
+                , RoomNumber, CheckIn.ToString("dd/MM/yyyy"), CheckOut.ToString("dd/MM/yyyy"), Duration());
+            return reservation;
+        }
+    }
+}
---
+++ exercicio_exceptions/exercicio_exceptions/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using exercicio_exceptions.Entities;
+using exercicio_exceptions.Entities.Exceptions;
+
+namespace exercicio_exceptions
+{
+     class Program
+    {
+        public static void Main()
+        {
+            try
+            {
+                Console.WriteLine("Número do Quarto: ");
+                var roomNumber = int.Parse(Console.ReadLine());
+                Console.WriteLine("Check-in: (dd/MM/aaaa)");
+                var checkin = DateTime.Parse(Console.ReadLine());
+                Console.WriteLine("Check-out: (dd/MM/aaaa)");
+                var checkout = DateTime.Parse(Console.ReadLine());
+                var reserv = new Reservation(roomNumber, checkin, checkout);
+                Console.WriteLine(reserv);
+
+                Console.WriteLine();
+                Console.WriteLine("Insira os dados para atualização da reserva: ");
+                Console.WriteLine("Check-in: (dd/MM/aaaa)");
+                var checkinUp = DateTime.Parse(Console.ReadLine());
+                Console.WriteLine("Check-out: (dd/MM/aaaa)");
+                var checkoutUp = DateTime.Parse(Console.ReadLine());
+                reserv.UpdateDate(checkinUp, checkoutUp);
+                Console.WriteLine(reserv);
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Erro na reserva: " + e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Erro de formatação: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro Inesperado: "+e.Message);
+            }
+        }
+    }
+}
