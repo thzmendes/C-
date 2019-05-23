093852499a96e84d86362973222cceded174f31a
tmendes <th.mendes1997@gmail.com>
2019-05-23 19:56:50 -0300

    Directory

---
+++ exercicio_Directory/exercicio_Directory/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace exercicio_Directory
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var path = @"C:\temp\Pasta";
+            try
+            {
+                var folderDirectories = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("Pastas: ");
+                foreach (var f in folderDirectories) Console.WriteLine(f);
+                var folderFiles = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("Pastas: ");
+                foreach (var f in folderFiles) Console.WriteLine(f);
+                Directory.CreateDirectory(path + @"\NewFolder");
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
