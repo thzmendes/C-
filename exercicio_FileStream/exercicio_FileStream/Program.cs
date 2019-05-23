81c8a6bef6f3daa8d053adbe2a444f831058f259
tmendes <th.mendes1997@gmail.com>
2019-05-23 18:11:46 -0300

    FileStream e StreamReader

---
+++ exercicio_FileStream/exercicio_FileStream/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace exercicio_FileStream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\temp\file1.txt";
+            FileStream fs = null;
+            StreamReader sr = null;
+            try
+            {
+                fs = new FileStream(path, FileMode.Open);
+                sr = new StreamReader(fs);
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+
+                    Console.WriteLine(line);    
+                }
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+                if (fs !=null) fs.Close();
+            }
+        }
+    }
+}
