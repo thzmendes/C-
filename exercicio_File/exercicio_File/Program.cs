f554075187a8dc7a0486c374910173160f042cd8
tmendes <th.mendes1997@gmail.com>
2019-05-23 17:52:32 -0300

    Exercicio manupilação arquivo

---
+++ exercicio_File/exercicio_File/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace exercicio_File
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var sourcePath = @"C:\temp\file1.txt";
+            var targetPath = @"C:\temp\file2.txt";
+            try
+            {
+                var fileInfo = new FileInfo(sourcePath);
+                //fileInfo.CopyTo(targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (var l in lines)
+                {
+                    Console.WriteLine(l);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+                
+            }
+        }
+    }
+}
