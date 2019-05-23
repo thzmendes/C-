d6f6cb8e632588306274113fba1154e89d347bd0
tmendes <th.mendes1997@gmail.com>
2019-05-23 18:40:32 -0300

    Bloco using StreamReader

---
+++ exercicio_BlocoUsing/exercicio_BlocoUsing/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace exercicio_BlocoUsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\temp\file1.txt";
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        Console.WriteLine(sr.ReadLine());
+                    }
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Erro: " + e.Message);
+
+            }
+
+
+
+
+
+        }
+    }
+}
