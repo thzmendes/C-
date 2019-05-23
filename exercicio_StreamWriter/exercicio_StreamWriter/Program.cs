b949134f1f750cdfafbf7bf4989848d76a196f1d
tmendes <th.mendes1997@gmail.com>
2019-05-23 18:54:03 -0300

    StreamWriter

---
+++ exercicio_StreamWriter/exercicio_StreamWriter/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace exercicio_StreamWriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\temp\file1.txt";
+            string targetPath = @"C:\temp\file3.txt";
+            try
+            {
+                string[] Lines = File.ReadAllLines(sourcePath);
+
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach (var line in Lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+               
+            }
+        }
+    }
+}
