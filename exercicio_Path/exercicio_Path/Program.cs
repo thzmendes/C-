008a7e3434111a176e100946c3d36bd610c2240d
tmendes <th.mendes1997@gmail.com>
2019-05-23 21:22:10 -0300

    Manipulação de Path

---
+++ exercicio_Path/exercicio_Path/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace exercicio_Path
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\temp\Pasta\Folder1\file1.txt";
+            Console.WriteLine(Path.GetDirectoryName(path));
+            Console.WriteLine(Path.PathSeparator);
+            Console.WriteLine(Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine(Path.GetExtension(path));
+            Console.WriteLine(Path.GetTempFileName());
+
+        }
+    }
+}
