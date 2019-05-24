e2086085a79941b566cb943634ed99c1f072c9db
tmendes <th.mendes1997@gmail.com>
2019-05-23 21:22:45 -0300

    Manipulação de arquivo CSV

---
+++ exercicio_FIles-Path-Stream-Directory/exercicio_FIles-Path-Stream-Directory/Entities/Product.cs
@@ -0,0 +1,26 @@
+﻿namespace exercicio_FIles_Path_Stream_Directory.Entities
+{
+    internal class Product
+    {
+        public Product(string name, double price, int quantidade)
+        {
+            Name = name;
+            Price = price;
+            Quantidade = quantidade;
+        }
+
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantidade { get; set; }
+
+        public double TotalValue()
+        {
+            return Price * Quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Name + " " + TotalValue().ToString("F2");
+        }
+    }
+}
---
+++ exercicio_FIles-Path-Stream-Directory/exercicio_FIles-Path-Stream-Directory/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using exercicio_FIles_Path_Stream_Directory.Entities;
+
+namespace exercicio_FIles_Path_Stream_Directory
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Insira o Path do Arquivo .csv ");
+            string path = Console.ReadLine();
+            var targetPath = Path.GetDirectoryName(path) + @"\out";
+            try
+            {
+                var list = File.ReadAllLines(path);
+                Directory.CreateDirectory(targetPath);
+                using (var sw = File.CreateText(targetPath + @"\summary.csv"))
+                {
+                    foreach (var l in list)
+                    {
+                        var product = l.Split(',');
+                        var name = product[0];
+                        var price = double.Parse(product[1]);
+                        var quantidade = int.Parse(product[2]);
+                        var p = new Product(name, price, quantidade);
+                        sw.WriteLine(p.ToString());
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Erro: " + e.Message);
+            }
+            catch (SystemException e)
+            {
+                Console.WriteLine("Erro: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro: " + e.Message);
+            }
+        }
+    }
+}
