e426193db2e58443475c5dc1f2f883c1c32716fe
Thomas <th.mendes1997@gmail.com>
2019-05-30 15:16:05 -0300

    Exercicio Extension Methods

---
+++ exercicio_ExtensionMethods/exercicio_ExtensionMethods/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+namespace System
+{
+    internal static class DateTimeExtensions
+    {
+        public static string ElapsedTime(this DateTime thisobj)
+        {
+            var duration = DateTime.Now - thisobj;
+
+            if (duration.TotalHours < 12)
+                return duration.TotalHours.ToString("F1", CultureInfo.InvariantCulture) + " horas";
+            return duration.TotalDays.ToString("F1", CultureInfo.InvariantCulture) + " dias";
+        }
+    }
+}
---
+++ exercicio_ExtensionMethods/exercicio_ExtensionMethods/Extensions/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace System
+{
+    static class StringExtensions
+    {
+        public static string Cut(this String str, int count)
+        {
+            if(str.Length <= count) return str;
+            return str.Substring(0, count)+"...";
+        }
+
+    }
+}
