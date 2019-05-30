b3884402ef8868aff88755997db19c86823f42b2
thzmendes <tmendes@icgti.com.br>
2019-05-30 14:52:29 -0300

    Exercicio IComparable

---
+++ exercicio_IComparable/exercicio_IComparable/Entities/Funcionario.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercicio_IComparable.Entities
+{
+    class Funcionario : IComparable
+    {
+        public string Nome {get;set;}
+        public double Salario {get;set;}
+
+        public Funcionario(string csvemployee)
+        {
+            string[] vect = csvemployee.Split(",");
+            Nome = vect[0];
+            Salario = double.Parse(vect[1], CultureInfo.InvariantCulture);
+        }
+
+        public override string ToString()
+        {
+            return Nome+", "+Salario.ToString("F2",CultureInfo.InvariantCulture);
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (!(obj is Funcionario))
+            {
+                throw new ArgumentException("Objeto não é um Funcionário");
+            }
+
+            Funcionario other = obj as Funcionario;
+            return Salario.CompareTo(other.Salario);
+        }
+    }
+}
---
+++ exercicio_IComparable/exercicio_IComparable/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using exercicio_IComparable.Entities;
+
+namespace exercicio_IComparable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\temp\in.csv";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<Funcionario> list = new List<Funcionario>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(new Funcionario(sr.ReadLine()));
+                    }
+
+                    list.Sort();
+                    foreach(var x in list){
+                        Console.WriteLine(x.ToString());
+                    }
+                }
+                
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Erro: " + e.Message);
+            }
+        }
+    }
+}
