a6edf26cb4203e97033dc4aaeb7784f5802ba558
tmendes <th.mendes1997@gmail.com>
2019-06-04 21:33:19 -0300

    exericio 3 linq

---
+++ exercicio_LINQ3/exercicio_LINQ3/Entities/Products.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace exercicio_LINQ3.Entities
+{
+    internal class Products
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Products(string name, double price)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Price = price;
+        }
+    }
+}
---
+++ exercicio_LINQ3/exercicio_LINQ3/Program.cs
@@ -0,0 +1,33 @@
+﻿using exercicio_LINQ3.Entities;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace exercicio_LINQ3
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string path = @"C:\temp\products.csv";
+            List<Products> products = new List<Products>();
+            using (StreamReader sr = File.OpenText(path))
+            {
+
+                while (!sr.EndOfStream)
+                {
+                    string[] product = sr.ReadLine().Split(",");
+                    products.Add(new Products(product[0],double.Parse(product[1],CultureInfo.InvariantCulture)));
+                }
+            }
+
+            var averagePrice = products.Select(p=>p.Price).DefaultIfEmpty(0.0).Average();
+            System.Console.WriteLine("Average Price: "+averagePrice.ToString("F2",CultureInfo.InvariantCulture));
+
+            var bellowAveragePrice = products.Where(p=> p.Price < averagePrice).OrderByDescending(p=> p.Name).Select(p=>p.Name);
+            foreach (string name in bellowAveragePrice) { System.Console.WriteLine(name); }
+
+        }
+    }
+}
