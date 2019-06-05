8c944df1d205d93a5b6e20f9de98eed0d5e1d77e
tmendes <th.mendes1997@gmail.com>
2019-06-04 22:02:01 -0300

    exericio 4 linq

---
+++ exercicio_LINQ4/exercicio_LINQ4/Entities/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace exercicio_LINQ4.Entities
+{
+    internal class Employee
+    {
+        public string Name {get;set;}
+        public string Email {get;set;}
+        public double Salary {get;set;}
+
+        public Employee(string name, string email, double salary)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Email = email ?? throw new ArgumentNullException(nameof(email));
+            Salary = salary;
+        }
+
+        public override string ToString()
+        {
+            return Name +", "+ Email +", "+Salary.ToString("F2");
+        }
+    }
+}
---
+++ exercicio_LINQ4/exercicio_LINQ4/Program.cs
@@ -0,0 +1,34 @@
+﻿using exercicio_LINQ4.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace exercicio_LINQ4
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string path = @"C:\temp\employee.csv";
+            List<Employee> listEmployees = new List<Employee>();
+            using (StreamReader sr = File.OpenText(path))
+            {
+                while(!sr.EndOfStream)
+                {
+                    string[] emp = sr.ReadLine().Split(",");
+                    listEmployees.Add(new Employee(emp[0],emp[1],double.Parse(emp[2],CultureInfo.InvariantCulture)));
+                }
+
+                double value = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+                var result = listEmployees.Where(p=> p.Salary > value).OrderBy(p=> p.Email).Select(p=> p.Email);
+                var resultSum = listEmployees.Where(p=> p.Name[0]=='M').Select(p=> p.Salary).Sum();
+                Console.WriteLine("Email of people with Salary > "+value);
+                foreach (var p in result) { Console.WriteLine(p);}
+                Console.WriteLine("Sum of salary of peoples whose name start with 'm': "+ resultSum);
+            }
+        }
+    }
+}
